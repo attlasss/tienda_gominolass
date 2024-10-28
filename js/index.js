f4e2458cb0d0de9ee975f9c508fb1b68d0559b71
carolina <carolynmortoby@gmail.com>
2024-10-28 10:59:29 +0100

    commit3

--- js/index.js
+++ js/index.js
@@ -1,82 +1,88 @@
 // obtener array de carrito, si no existe se crea un array.
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+// obtener contador de la cesta.
+let contador_cesta = localStorage.getItem("contador_cesta") || 0;
+contador_cesta = parseInt(contador_cesta) || 0;
 
 function addToCart(nombre, idCantidad) {
-    let precio;
-    // obtener el numero de cantidad
-    let cantidad = document.getElementById(idCantidad).value;
-    // switch para asignar el precio de los items, con una bbdd lo
-    // obtendriamos de la misma bbdd
-    switch (nombre) {
-        case "Ositos":
-            precio = 1;
-            break;
+  let precio;
+  // obtener el numero de cantidad
+  let cantidad = document.getElementById(idCantidad).value;
+  // switch para asignar el precio de los items, con una bbdd lo
+  // obtendriamos de la misma bbdd
+  switch (nombre) {
+    case "Ositos":
+      precio = 1;
+      break;
 
-        case "Fresas":
-            precio = 2;
-            break;
+    case "Fresas":
+      precio = 2;
+      break;
 
-        case "Tiburones":
-            precio = 3;
-            break;
+    case "Tiburones":
+      precio = 3;
+      break;
 
-        case "Sandias":
-            precio = 4;
-            break;
+    case "Sandias":
+      precio = 4;
+      break;
 
-        case "Ferrero":
-            precio = 10;
-            break;
+    case "Ferrero":
+      precio = 10;
+      break;
 
-        case "Oreo":
-            precio = 2;
-            break;
+    case "Oreo":
+      precio = 2;
+      break;
 
-        case "Conguitos":
-            precio = 2;
-            break;
+    case "Conguitos":
+      precio = 2;
+      break;
 
-        case "MMs":
-            precio = 1;
-            break;
+    case "MMs":
+      precio = 1;
+      break;
 
-        default:
-            console.log("Tipo de chuche no reconocido.");
-            break;
+    default:
+      console.log("Tipo de chuche no reconocido.");
+      break;
+  }
+
+  try {
+    let productoExistente = carrito.find(producto => producto.nombre === nombre);
+    contador_cesta = parseInt(contador_cesta) || 0;
+
+    if (productoExistente) {
+        let cantidadAnterior = productoExistente.cantidad;
+        productoExistente.cantidad += cantidad;
+        productoExistente.precioTotal = productoExistente.cantidad * precio;
+        contador_cesta += cantidad;
+    } else {
+        let producto = {
+            nombre: nombre,
+            cantidad: cantidad,
+            precio: precio,
+            precioTotal: precio * cantidad
+        };
+        carrito.push(producto);
+        contador_cesta += cantidad;
     }
+    
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    document.getElementById('items').innerHTML = contador_cesta;
+    localStorage.setItem("contador_cesta", contador_cesta);
 
-    let precioTotal = precio * cantidad;
-
-    try {
-        let productoExistente = carrito.find(producto => producto.nombre === nombre);
-        if (productoExistente) {
-            // Si el producto ya está en el carrito, actualizamos la cantidad y el precio total
-            productoExistente.cantidad = cantidad;
-            productoExistente.precioTotal = productoExistente.cantidad * precio;
-        } else {
-            // Si no existe, creamos el producto con la cantidad que nos pasan
-            let producto = {
-                nombre: nombre,
-                cantidad: cantidad,
-                precio: precio,
-                precioTotal: precioTotal
-            }
-            console.log(producto);
-            carrito.push(producto);
-        }
-        
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-
-    } catch (error) {
-        console.error(error);
-        alert("Ha habido algun error");
-    }
+} catch (error) {
+    console.error(error);
+    alert("Ha habido algún error");
+}
 }
 
+
 function cesta() {
-    location.href = "cesta.html";
+  location.href = "cesta.html";
 }
 
 function login() {
-    location.href = "login.html";
+  location.href = "login.html";
 }
